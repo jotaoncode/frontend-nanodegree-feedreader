cf3d171461fb5fc72d89eaa137d245b6d2bab188
jotaoncode <garcia_juan_j@yahoo.com.ar>
2015-10-22 23:39:08 -0300

    Added some test I was missing for check the feed changes four times  I call feed for content. And also modified test for checking transformations in menu by default.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -21,7 +21,7 @@ $(function() {
          * allFeeds in app.js to be an empty array and refresh the
          * page?
          */
-        it('are defined', function() {
+        it('Are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
@@ -32,7 +32,7 @@ $(function() {
          * and that the URL is not empty.
          */
 
-        it('all url in feeds are defined', function () {
+        it('All url in feeds are defined', function () {
           for(var feed in allFeeds) {
             expect(allFeeds[feed].url).toBeDefined();
             expect(allFeeds[feed].url).not.toBe("");
@@ -44,7 +44,7 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-        it('all name in feeds are defined', function () {
+        it('All name in feeds are defined', function () {
           for(var feed in allFeeds) {
             expect(allFeeds[feed].name).toBeDefined();
             expect(allFeeds[feed].name).not.toBe("");
@@ -57,23 +57,23 @@ $(function() {
     describe('The Menu', function () {
       var menu;
       beforeEach(function () {
-        menu = $('.menu-hidden>.menu');
+        body = $('body');
+        menu = $('body>.menu');
       });
       /* Write a test that ensures the menu element is
        * hidden by default. You'll have to analyze the HTML and
        * the CSS to determine how we're performing the
        * hiding/showing of the menu element.
        */
-      it('should be hidden by default', function () {
-        expect(menu.css('transform')).toBe('matrix(1, 0, 0, 1, -192, 0)');
-        expect(menu.css('transition')).toBe('transform 0.2s ease 0s');
+      it('Should be hidden by default', function () {
+        expect(body.hasClass('menu-hidden')).toBe(true);
       });
      /* Write a test that ensures the menu changes
       * visibility when the menu icon is clicked. This test
       * should have two expectations: does the menu display when
       * clicked and does it hide when clicked again.
       */
-      describe('menu open and closes', function () {
+      describe('Menu open and closes', function () {
         var timerCallback, menuLink;
         beforeEach(function () {
           menuLink = $('.menu-icon-link');
@@ -84,19 +84,18 @@ $(function() {
           jasmine.clock().uninstall();
         });
 
-        it('should display when clicked', function () {
-          menuLink.trigger('click');
-          timerCallback();
-          jasmine.clock().tick(220);
-          expect(timerCallback).toHaveBeenCalled();
-          expect(menu.css('transform')).toBe('matrix(1, 0, 0, 1, -192, 0)');
+        it('Should display when clicked', function () {
+          menuLink.trigger('click'); //Triggered a click event on the menu
+          timerCallback();//Runned timerCallback to defer the transformation
+          jasmine.clock().tick(220);//When it finishes
+          expect(body.hasClass('menu-hidden')).toBe(false); //Transformation should have taken effect
         });
-        it('should hide when clicked again', function () {
+        it('Should hide when clicked again', function () {
           menuLink.trigger('click');
           timerCallback();
           jasmine.clock().tick(220);
           expect(timerCallback).toHaveBeenCalled();
-          expect(menu.css('transform')).not.toBeDefined();
+          expect(body.hasClass('menu-hidden')).toBe(true); //Transformation should have taken effect
         });
       });
     });
@@ -111,32 +110,86 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test wil require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-      it('load feeds asynchronous', function (done) {
+      it('Load feeds asynchronous', function (done) {
         expect($('.entry').length).not.toBe(0);
         done();
       });
     });
     /* Write a new test suite named "New Feed Selection*/
     describe('New Feed Selection', function () {
-      beforeEach(function (done) {
-        loadFeed(0, done);
-      });
-
+      var contentBefore;
       /* Write a test that ensures when a new feed is loaded
        * by the loadFeed function that the content actually changes.
        * Remember, loadFeed() is asynchronous.
        */
-      it('feed is loaded with content', function (done) {
-        var entries = $('.feed').find('.entry'), entryValue;
-        entries.each(function (index, entry) {
-          expect($(entry).find('h2').text()).toBeDefined();
-          expect($(entry).find('h2').text()).not.toBe("");
-          expect($(entry).find('p').text()).toBeDefined();
-          expect($(entry).find('p').text()).not.toBe("");
+      it('Feed is loaded with content for first feed', function (done) {
+        loadFeed(0, function () {
+          var entries = $('.feed').find('.entry'), entryValue;
+          entries.each(function (index, entry) {
+            expect($(entry).find('h2').text()).toBeDefined();
+            expect($(entry).find('h2').text()).not.toBe("");
+            expect($(entry).find('p').text()).toBeDefined();
+            expect($(entry).find('p').text()).not.toBe("");
+            contentBefore = $(entry).find('h2').text();
+          });
+          done();
         });
-        done();
       });
-      it('there should be as many entries as the ones in the list feed we require', function (done) {
+      /**
+       * Feed test to check second feed is loaded
+       */
+      it('Feed is loaded with content for second feed', function (done) {
+        loadFeed(1, function () {
+          var entries = $('.feed').find('.entry'), entryValue;
+          entries.each(function (index, entry) {
+            expect($(entry).find('h2').text()).toBeDefined();
+            expect($(entry).find('h2').text()).not.toBe("");
+            expect($(entry).find('h2').text()).not.toBe(contentBefore);
+            expect($(entry).find('p').text()).toBeDefined();
+            expect($(entry).find('p').text()).not.toBe("");
+            contentBefore = $(entry).find('h2').text();
+          });
+          done();
+        });
+      });
+      /**
+       * Feed test to check third feed is loaded
+       */
+      it('Feed is loaded with content for third feed', function (done) {
+        loadFeed(2, function () {
+          var entries = $('.feed').find('.entry'), entryValue;
+          entries.each(function (index, entry) {
+            expect($(entry).find('h2').text()).toBeDefined();
+            expect($(entry).find('h2').text()).not.toBe("");
+            expect($(entry).find('h2').text()).not.toBe(contentBefore);
+            expect($(entry).find('p').text()).toBeDefined();
+            expect($(entry).find('p').text()).not.toBe("");
+            contentBefore = $(entry).find('h2').text();
+          });
+          done();
+        });
+      });
+      /**
+       * Feed test to check fourth feed is loaded
+       */
+      it('Feed is loaded with content for fourth feed', function (done) {
+        loadFeed(3, function () {
+          var entries = $('.feed').find('.entry'), entryValue;
+          entries.each(function (index, entry) {
+            expect($(entry).find('h2').text()).toBeDefined();
+            expect($(entry).find('h2').text()).not.toBe("");
+            expect($(entry).find('h2').text()).not.toBe(contentBefore);
+            expect($(entry).find('p').text()).toBeDefined();
+            expect($(entry).find('p').text()).not.toBe("");
+            contentBefore = $(entry).find('h2').text();
+          });
+          done();
+        });
+      });
+      /**
+       * There should be 4 entries, the same as the length of allFeeds object
+       */
+      it('There should be as many entries as the ones in the list feed we require', function (done) {
         var entries = $('.feed').find('.entry');
         expect(entries.length).toBe(allFeeds.length);
         done();
